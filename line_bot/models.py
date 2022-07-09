2b190d9a5a1908c1e34a996ad64fc13e812761e5
tsukaryo <ohtsukaryoji@gmail.com>
2022-07-09 14:48:46 +0900

    適当

--- line_bot/models.py
+++ line_bot/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Place:
+    user_id = models.IntegerField()
+    name = models.CharField(max_length=200)
+    url = models.CharField(max_length=200)
+
+    
--- line_bot/views.py
+++ line_bot/views.py
@@ -5,7 +5,7 @@ from django.views.decorators.csrf import csrf_exempt
 
 from .utils import message_creater
 from .line_message import LineMessage
-
+from .models import Place
 
 
 @csrf_exempt
@@ -16,7 +16,18 @@ def index_view(request):
         data = request['events'][0]
         message = data['message']
         reply_token = data['replyToken']
-
         line_message = LineMessage(message_creater.create_single_text_message(message['text']))
-        line_message.reply(reply_token)
+        if message['text'] == "保存して":
+            # Place.objects.create(name='Taro', url='Hello, World!')
+            line_message = LineMessage(message_creater.create_single_text_message("何を保存しますか"))
+            line_message.reply(reply_token)
+            return HttpResponse("ok")
+        elif message['text'] == "表示して":
+            line_message = LineMessage(message_creater.create_single_text_message("何を表示しますか"))
+            line_message.reply(reply_token)
+            # Place.objects.filter(user_id=1)
+            return HttpResponse("ok")
+        
+        print(line_message)
+        
     return HttpResponse("ok")

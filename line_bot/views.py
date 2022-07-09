ca98d34090f3d4fbe0283e43b5fb4e3d4b6a5ae1
tsukaryo <ohtsukaryoji@gmail.com>
2022-07-10 00:30:32 +0900

    適当

--- line_bot/views.py
+++ line_bot/views.py
@@ -193,9 +193,8 @@ def db_register_url(reply_token,message):
     place_data.url = recieved_url
     status.save()
     place_data.save()
-    send_text = "カテゴリーを入力してください"
-    line_message_send = LineMessage(message_creater.create_single_text_message(send_text))
-    line_message_send.reply(reply_token)
+    select_category = CategorySelect()
+    select_category.CS_reply_register(reply_token)
     
     # line_category_register = CategorySelect()
     # line_category_register.CS_reply_register(reply_token)

cadc490e2e901e15740ea704960a071ef92052be
tsukaryo <ohtsukaryoji@gmail.com>
2022-07-09 17:34:23 +0900

    適当

--- line_bot/admin.py
+++ line_bot/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from . models import Place
+from . models import Place,Status
 # Register your models here.
 
 admin.site.register(Place)
+admin.site.register(Status)

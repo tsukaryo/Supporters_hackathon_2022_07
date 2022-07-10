3959aa94b2fd673d1855780c3d09477d2c617166
tsukaryo <ohtsukaryoji@gmail.com>
2022-07-10 13:03:13 +0900

    Day1 final

--- line_bot/admin.py
+++ line_bot/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from . models import Place,Status
+from . models import Place,Status,Category
 # Register your models here.
 
 admin.site.register(Place)
 admin.site.register(Status)
+admin.site.register(Category)
